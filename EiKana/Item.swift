0c2c7e93a8b40ac7df686e2b8c19d750db38a603
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-24 20:32:21 +0900

    w

--- EiKana/Item.swift
+++ EiKana/Item.swift
@@ -1,17 +1,10 @@
-//
-//  Item.swift
-//  EiKana
-//
-//  Created by Koji Kuniyoshi on 2025-05-19.
-//
-
 import Foundation
 import SwiftData
 
 @Model
 final class Item {
     var timestamp: Date
-    
+
     init(timestamp: Date) {
         self.timestamp = timestamp
     }
