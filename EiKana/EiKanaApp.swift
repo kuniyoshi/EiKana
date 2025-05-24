1d0d82c8224815557e1c4a9e940b4b8f788b6073
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-24 20:31:58 +0900

    w

--- EiKana/EiKanaApp.swift
+++ EiKana/EiKanaApp.swift
@@ -4,7 +4,7 @@ import SwiftData
 @main
 struct EiKanaApp: App {
     private let imeManager = IMEManager()
-    
+
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self
