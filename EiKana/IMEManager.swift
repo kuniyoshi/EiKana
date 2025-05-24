6ec14f0389c1d0d522433a24e70967fb58290482
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-24 11:49:10 +0900

    reacts command key

--- EiKana/IMEManager.swift
+++ EiKana/IMEManager.swift
@@ -3,6 +3,8 @@ import Carbon
 import Carbon.HIToolbox
 
 final class IMEManager {
+    private var runLoopSource: CFRunLoopSource?
+
     private static func callback(
         proxy: CGEventTapProxy,
         type: CGEventType,
@@ -10,10 +12,29 @@ final class IMEManager {
         refcon: UnsafeMutableRawPointer?
     ) -> Unmanaged<CGEvent>? {
         let imeManager = Unmanaged<IMEManager>.fromOpaque(refcon!).takeUnretainedValue()
+        if type == .flagsChanged {
+            let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+            print("flags", keyCode)
+            let flags = event.flags
+
+            // Left Command key code is 0x37 on macOS
+            let leftCommandKeyCode: CGKeyCode = 0x37
+            if keyCode == leftCommandKeyCode && flags.contains(.maskCommand) {
+                imeManager.switchToEisuMode()
+                return nil
+            }
+
+            // Right Command key code is 0x36 on macOS
+            let rightCommandKeyCode: CGKeyCode = 0x36
+            if keyCode == rightCommandKeyCode && flags.contains(.maskCommand) {
+                imeManager.switchToKanaMode()
+                return nil
+            }
+        }
         // Only interested in keyDown events
         if type == .keyDown {
             let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
-            print(keyCode)
+            print("key", keyCode)
             let flags = event.flags
             // Left Command key code is 0x37 on macOS
             let leftCommandKeyCode: CGKeyCode = 0x37
@@ -43,14 +64,16 @@ final class IMEManager {
     }
 
     deinit {
-//        CFRunLoopRemoveSource(CFRunLoopGetCurrent(), queue, .commonModes)
+        if let runLoopSource = runLoopSource {
+            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+        }
         if let eventTap = eventTap {
             CFMachPortInvalidate(eventTap)
         }
     }
 
     private func setupEventTap() {
-        let mask = CGEventMask(1 << CGEventType.keyDown.rawValue)
+        let mask = CGEventMask(1 << CGEventType.keyDown.rawValue) | CGEventMask(1 << CGEventType.flagsChanged.rawValue)
         eventTap = CGEvent.tapCreate(
             tap: .cghidEventTap,
             place: .headInsertEventTap,
@@ -63,19 +86,23 @@ final class IMEManager {
         )
 
         if let eventTap = eventTap {
-            let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
-            CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+            if let runLoopSource = runLoopSource {
+                CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            }
             CGEvent.tapEnable(tap: eventTap, enable: true)
         }
     }
 
     private func switchToEisuMode() {
+        print("set eisu mode")
         let event = CGEvent(source: nil)
         event?.setIntegerValueField(.keyboardEventKeycode, value: 0x30) // 0x30 is the key code for switching to eisu mode
         event?.post(tap: .cghidEventTap)
     }
 
     private func switchToKanaMode() {
+        print("set kana mode")
         let event = CGEvent(source: nil)
         event?.setIntegerValueField(.keyboardEventKeycode, value: 0x31) // 0x31 is the key code for switching to kana mode
         event?.post(tap: .cghidEventTap)
