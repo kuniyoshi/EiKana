8a7cf9a3021b048dc4867a8f4b5e5b5a33af0878
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-26 20:39:20 +0900

    selectable modifier

--- EiKana/ContentView.swift
+++ EiKana/ContentView.swift
@@ -3,16 +3,17 @@ import SwiftData
 import Foundation
 
 struct ContentView: View {
-    @AppStorage("userName") private var userName: String = ""
+    @AppStorage("modifierKeyType") private var modifierKeyType: String = "control"
     @Environment(\.modelContext) private var modelContext
 
     var body: some View {
         VStack {
-            Text("ようこそ、\(userName)さん！")
-                .font(.headline)
-            TextField("ユーザー名を入力", text: $userName)
-                .textFieldStyle(.roundedBorder)
-                .padding()
+            Picker("切り替えキー", selection: $modifierKeyType) {
+                Text("Control").tag("control")
+                Text("Command").tag("command")
+            }
+            .pickerStyle(.segmented)
+            .padding()
         }
         .padding()
     }
